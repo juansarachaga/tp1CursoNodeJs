27d04f9c4d786f464aa1b73ff7b12c5ae5ec24b0
Juan Saráchaga <juan.sarachaga@gmail.com>
2022-10-03 19:51:06 -0300

    config o index .js???

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -3,9 +3,10 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
@@ -33,7 +34,5 @@ Object.keys(db).forEach(modelName => {
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-
-sequelize.sync();
 
 module.exports = db;
